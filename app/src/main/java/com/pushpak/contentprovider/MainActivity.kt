5d25f58dd48f2b19e796f81bbce62ebb608e810a
Pushpak Gosavi <pushpak.gosavi7@gmail.com>
2024-05-27 18:10:02 +0530

    Initial Commit

---
+++ app/src/main/java/com/pushpak/contentprovider/MainActivity.kt
@@ -0,0 +1,93 @@
+package com.pushpak.contentprovider
+
+import android.Manifest
+import android.content.ContentUris
+import android.net.Uri
+import android.os.Bundle
+import android.provider.MediaStore
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.core.app.ActivityCompat
+import coil.compose.AsyncImage
+import com.pushpak.contentprovider.ui.theme.ContentProviderTheme
+import java.util.Calendar
+
+class MainActivity : ComponentActivity() {
+
+    private val viewModel by viewModels<ImageViewModel>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(Manifest.permission.READ_MEDIA_IMAGES),
+            0
+        )
+
+        val projection = arrayOf(
+            MediaStore.Images.Media._ID,
+            MediaStore.Images.Media.DISPLAY_NAME
+        )
+        val millisYesterday = Calendar.getInstance().apply {
+            add(Calendar.DAY_OF_YEAR, -1)
+        }.timeInMillis
+        val selection = "${MediaStore.Images.Media.DATE_TAKEN} >= ?"
+        val selectionArgs = arrayOf(millisYesterday.toString())
+
+        val sortOrder = "${MediaStore.Images.Media.DATE_TAKEN} DESC"
+        contentResolver.query(
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+            projection,
+            selection,
+            selectionArgs,
+            sortOrder
+        )?.use { cursor ->
+            val idColumn = cursor.getColumnIndex(MediaStore.Images.Media._ID)
+            val nameColumn = cursor.getColumnIndex(MediaStore.Images.Media.DISPLAY_NAME)
+            val images = mutableListOf<Image>()
+            while (cursor.moveToNext()) {
+                val id = cursor.getLong(idColumn)
+                val name = cursor.getString(nameColumn)
+                val uri = ContentUris.withAppendedId(
+                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                    id
+                )
+                images.add(Image(id, name, uri))
+            }
+            viewModel.updateImages(images)
+        }
+        setContent {
+            ContentProviderTheme {
+                LazyColumn(
+                    modifier = Modifier.fillMaxSize(),
+                    content = {
+                        items(viewModel.images) { image ->
+                            Column(
+                                modifier = Modifier.fillMaxWidth(),
+                                horizontalAlignment = Alignment.CenterHorizontally
+                            ) {
+                                AsyncImage(model = image.uri, contentDescription = null)
+                                Text(text = image.name)
+                            }
+                        }
+                    })
+            }
+        }
+    }
+}
+
+data class Image(
+    val id: Long,
+    val name: String,
+    val uri: Uri
+)
